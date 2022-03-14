18fcdfde0d994d7aa126fdba98e1804e06dc58d6
Geraldo Júnior <geraldojuni@hotmail.com>
2022-03-13 23:48:03 -0300

    fix: configure express.json on server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import routes from './routes';
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is listening on port ${process.env.PORT}.`);
