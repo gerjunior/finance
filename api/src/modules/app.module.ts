dd776d0b6794c0d8d351b52e6e4ffb997715a8a9
Geraldo Júnior <geraldojuni@hotmail.com>
2022-10-28 13:58:57 -0300

    chore: create transactions module structure

--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [TransactionsModule],
   controllers: [],
   providers: [],
 })
